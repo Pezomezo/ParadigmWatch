1c99c8a2f5b92716c06c5d2e1f10e033e73b8ea9
Ádám Horváth <pezolino19@gmail.com>
2020-05-12 16:20:10 +0200

    Add project files.

---
+++ ParadigmWatch/Startup.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ParadigmWatch
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // WE WILL ENABLES THIS AGAIN ONCE WE HAVE A DATABASE READY :)
+            //services.AddDbContext<AppIdentityDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("IdentityConnection")));
+
+
+            //services.AddIdentity<AppUser, IdentityRole>(opts => {
+            //    opts.User.RequireUniqueEmail = true;
+            //    //opts.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyz";
+            //    opts.Password.RequiredLength = 6;
+            //    opts.Password.RequireNonAlphanumeric = false;
+            //    opts.Password.RequireLowercase = false;
+            //    opts.Password.RequireUppercase = false;
+            //    opts.Password.RequireDigit = false;
+            //}).AddEntityFrameworkStores<AppIdentityDbContext>()
+            //.AddDefaultTokenProviders();
+
+            //services.ConfigureApplicationCookie(opts => opts.LoginPath = "/Users/Login");
+
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseAuthentication();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            //AppIdentityDbContext.CreateAdminAccount(app.ApplicationServices, Configuration).Wait();
+        }
+    }
+}
