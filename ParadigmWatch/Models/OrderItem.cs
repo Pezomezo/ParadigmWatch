fed3fa337cafda4ebf39f6bb622f6e82158812dd
Ádám Horváth <pezolino19@gmail.com>
2020-05-13 16:51:12 +0200

    started with database

---
+++ ParadigmWatch/Models/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParadigmWatch.Models
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public int WatchID { get; set; }
+        public Watch Watch { get; set; }
+        public int Quanity { get; set; }
+        [Column(TypeName = "float")]
+        public decimal TotalPrice { get; set; }
+    }
+}
