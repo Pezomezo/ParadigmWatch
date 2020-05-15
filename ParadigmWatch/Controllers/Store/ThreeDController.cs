f3bfc3c581cbba9b11a06e62ca6f5eb722d9579f
Robert Csöllei <robertcsollei@gmail.com>
2020-05-15 19:41:17 +0200

    Three.js

---
+++ ParadigmWatch/Controllers/Store/ThreeDController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ParadigmWatch.Models;
+using ParadigmWatch.Infrastructure;
+
+namespace ParadigmWatch.Controllers.Store
+{
+    public class ThreeDController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ParadigmWatch/wwwroot/js/ProductPage.js
@@ -0,0 +1,6 @@
+﻿function PathFormat() {
+    let loc = document.getElementById('location')
+    let newLoc = loc.textContent.split('/').join(' > ')
+    loc.textContent = newLoc;
+}
+PathFormat()
