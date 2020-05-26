ef767eeec61606f4c6d1ebf1fa2fb0719125edd2
eaaprf <60609318+eaaprf@users.noreply.github.com>
2020-05-26 19:48:52 +0200

    The About, and Contact with Google Maps works. Favorits is setup.

--- ParadigmWatch/wwwroot/js/main.js
+++ ParadigmWatch/wwwroot/js/main.js
@@ -158,3 +158,13 @@ function redirect(urlMiddle) {
     }
     window.location.href = theUrl;
 }
+
+//Google Maps 
+
+   var map;
+        function initMap() {
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: { lat: 10.158886, lng: 56.119607 },
+                zoom: 1
+            });
+        }
